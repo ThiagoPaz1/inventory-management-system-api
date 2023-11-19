9a1974c0c9dda64b39ecdf9195831433be4c4c50
Thiago da Paz <thiagopazjunior@gmail.com>
2023-11-19 15:16:46 -0300

    feat

--- src/modules/product/product.repository.ts
+++ src/modules/product/product.repository.ts
@@ -1,5 +1,10 @@
 import { PrismaService } from "src/prisma/config";
+import { CreateProductDto } from "./dto/create-product.dto";
 
 export class ProductRepository {
-  constructor (private prismaService: PrismaService) {}
+  constructor(private prismaService: PrismaService) {}
+
+  async create(data: CreateProductDto) {
+    this.prismaService.product.create({ data });
+  }
 }
